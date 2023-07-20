978b5c0f11a2d238c8989eb8d020a7fe7f35e362
AdoNoColor <macccich@gmail.com>
2023-07-20 19:38:23 +0300

    fix: jwt auth methods

--- server/src/controller/UserController.ts
+++ server/src/controller/UserController.ts
@@ -9,10 +9,85 @@ export class UserController {
 
     private userRepository = AppDataSource.getRepository(User)
 
-    private createToken = (id) => {
-        const jwtkey = process.env.JWT;
-        return jwt.sign(id, jwtkey)
+    private createAccessToken = (user) => {
+        return jwt.sign({user}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d'});
     }
+
+    private authenticateToken = (req, res, next) => {
+        const authHeader = req.headers['authorization']
+        const token = authHeader && authHeader.split(' ')[1]
+        if (!token) return res.sendStatus(401)
+
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+            if (err) return res.status(403)
+            req.user = user
+            next()
+        })
+    }
+
+    async login(request: Request, response: Response) {
+        const { email, password } = request.body;
+        let user = await  this.userRepository.findOneBy({email : email});
+
+        try {
+            if (!user || !await bcrypt.compare(password, user.password)) {
+                return response.status(400).send({
+                    message : 'Invalid email or password!'
+                });
+            }
+
+            const accessToken = this.createAccessToken(user);
+            return response.status(200).json({accessToken});
+        } catch(error) {
+            return error;
+        }
+    }
+
+    async register(request: Request, response: Response) {
+        try {
+            const { name, email, password } = request.body;
+            if (await this.userRepository.findOneBy({email: email})) {
+                return response.status(400).send({
+                    message : 'Email like this exists!'
+                });
+            }
+
+            if (!name || !email || !password) {
+                return response.status(400).send({
+                    message : 'All fields are required!'
+                });
+            }
+
+            if (!validator.isEmail(email)) {
+                return response.status(400).send({
+                    message : 'Invalid email!'
+                });
+            }
+
+            if (!validator.isStrongPassword(password)) {
+                return response.status(400).send({
+                    message : 'Weak password!'
+                });
+            }
+
+            const salt = await bcrypt.genSalt(10);
+
+            let user = Object.assign(new User(), {
+                name,
+                email,
+                password,
+            })
+
+            user.password = await bcrypt.hash(user.password, salt)
+
+            await this.userRepository.save(user);
+            const accessToken = this.createAccessToken(user);
+            return response.status(200).json({accessToken})
+        } catch(error) {
+            return error.message;
+        }
+    }
+
 
     async all(request: Request, response: Response) {
         return this.userRepository.find()
@@ -30,59 +105,6 @@ export class UserController {
             return "unregistered user"
         }
         return user
-    }
-
-    async register(request: Request, response: Response) {
-        try {
-            const { name, email, password } = request.body;
-            if (await this.userRepository.findOneBy({email: email})) {
-                return response.status(400).json('User with the same email already exists!');
-            }
-
-            if (!name || !email || !password) {
-                return response.status(400).json('All fields are required!');
-            }
-
-            if (!validator.isEmail(email)) {
-                return response.status(400).json('Invalid email!');
-            }
-
-            if (!validator.isStrongPassword(password)) {
-                return response.status(400).json('Weak password!');
-            }
-
-            const salt = await bcrypt.genSalt(10);
-
-            let user = Object.assign(new User(), {
-                name,
-                email,
-                password,
-            })
-
-            user.password = await bcrypt.hash(user.password, salt)
-
-            await this.userRepository.save(user);
-            const token = this.createToken(user.id);
-            return response.status(200).json({id: user.id, name, email, token});
-        } catch(error) {
-            return error;
-        }
-    }
-
-    async login(request: Request, response: Response) {
-        const { email, password } = request.body;
-        let user = await  this.userRepository.findOneBy({email : email});
-
-        try {
-            if (!user || !await bcrypt.compare(password, user.password)) {
-                return response.status(400).json('Invalid email or password!');
-            }
-
-            const token = this.createToken(user.id);
-            return response.status(200).json({id: user.id, name: user.name, email, token});
-        } catch(error) {
-            return error;
-        }
     }
 
     async remove(request: Request, response: Response) {
