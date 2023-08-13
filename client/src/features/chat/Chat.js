39d6a4cbd350677046346b6a7afdf1da607f4a71
AdoNoColor <macccich@gmail.com>
2023-08-14 00:54:18 +0300

    feat: main page redirection

--- client/src/features/chat/Chat.js
+++ client/src/features/chat/Chat.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, {useEffect} from "react";
 import '../../styles/topBar.scss'
 import {useSelector} from "react-redux";
 import {selectCurrentToken, selectCurrentUser} from "../auth/authSlice";
+import {useNavigate} from "react-router-dom";
 export const Chat = () => {
     const user = useSelector(selectCurrentUser);
-    const content = user !== null ?
-        (<>you are logged in as {user}</>) :
-        (<>Placeholder</>)
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (user === null) {
+            navigate('/login');
+        }}, [])
 
+    const content =
+        (<>you are logged in as {user}</>)
     return content;
 }
 
